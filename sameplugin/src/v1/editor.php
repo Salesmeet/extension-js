a65cbe7d01766ba1b4715520ba1adab6e95bbc8f
Corrado <corradofacchini@MBP-di-Corrado.homenet.telecomitalia.it>
2022-05-11 15:29:32 +0200

    editor - all participant

--- sameplugin/src/v1/editor.php
+++ sameplugin/src/v1/editor.php
@@ -316,6 +316,8 @@
           for(i = 0; i < same_partecipant_list.length; i++) {
               valori += "<a style='" + stylea + "' href='#' onclick='parent.sameTooltipRapidCommand(\"" + same_partecipant_list[i].value + "\", \"@\", \"participant\");'>" + same_partecipant_list[i].value + "</a><br>";
           }
+          valori += "<a style='" + stylea + "' href='#' onclick='parent.sameTooltipRapidCommand(\"All participant\", \"@\", \"participant\");'>All participant</a><br>";
+
         }
         valori += "<hr><a style='" + stylea + "' href='#'  onclick='parent.sameTooltipRapidCommand(\"addparticipant\", \"addparticipant\", \"addparticipant\");'>Add participant</a>";
 
